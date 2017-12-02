c02de5506d1754aeb030e51c800dbc64a432e70b
ValLR <valentinalopezrecabal@gmail.com>
2017-12-02 10:25:05 -0300

    cambios en la función add y clear

--- static/js/apps/main.js
+++ static/js/apps/main.js
@@ -16,15 +16,58 @@ $(document).ready(function() {
 						<a class="waves-effect waves-light btn add-button" id="`+ el.id +`"><i class="material-icons left">add_shopping_cart</i>Add to Cart</a>
 					</div>
 		`
+		var allProductsHide = `
+
+					<div class="center-align col l2 m2 s6 product-box hide `+ el.id +`" id="`+ el.id +`hide">
+						<img src="`+ el.imageURL +`" type="image" alt="`+ el.id +`">
+						<p><b>`+ el.name +`</p>
+						<p>`+ el.currency + el.price+`</b></p>
+						<a class="waves-effect waves-light btn clear-button"><i class="material-icons left">clear</i>Remove</a>
+					</div>
+		`		
 		allProducts = $(allProducts);
 		$("#product-container").append(allProducts);
+		$("#cartProducts").append(allProductsHide);
 
 
 	});
-
+	$(".clear-button").click(function(){
+			var element = $(this).parent();
+			var id = $(element).attr("id");
+			var id_show = $("#"+id).attr("class").split(" ").pop();
+			var parentElement= $("#"+id_show).parent();
+			$(parentElement).removeClass("hide");
+			$("#"+id).addClass("hide");		
+	});
 
 	$(".add-button").click(function(){
 		var element = $(this).parent();
+		var id = $(this).attr("id");
+		$(element).addClass("hide");
+		$("#"+id+"hide").removeClass("hide");
+		if(id==="1"){
+			value += 250;
+			$("#value").text(value);
+		}
+		if(id==="2"){
+			value += 350;
+			$("#value").text(value);
+		}
+		if(id==="3"){
+			value += 200;
+			$("#value").text(value);
+		}
+		if(id==="4"){
+			value += 500;
+			$("#value").text(value);
+		}
+		if(id==="5"){
+			value += 100;
+			$("#value").text(value);
+		}						
+
+
+/*		var element = $(this).parent();
 		$("#cartProducts").append(element);
 		var id= $(this).attr("id");
 		$("#"+ id).removeClass("add-button").addClass("remove-button");
@@ -32,7 +75,7 @@ $(document).ready(function() {
 		$("#"+ id).append(`<i class="material-icons left">clear</i>Remove`);
 		if(id==="1"){
 			value += 250;
-			$("#value").text(value);
+			$("#value").text(value);/*
 			$(".remove-button").click(function(){
 				var element = $(this).parent();
 				$("#product-container").append(element);
@@ -40,7 +83,7 @@ $(document).ready(function() {
 				$("#"+ id).text("");
 				$("#"+ id).append(`<i class="material-icons left">add_shopping_cart</i>Add to cart`);				
 				value -= 250;
-				$("#value").text(value);	
+				$("#value").text(value);
 			});
 		}
 		else if(id==="2"){
@@ -94,7 +137,7 @@ $(document).ready(function() {
 				value -= 100;
 				$("#value").text(value);	
 			});
-		}
+		}*/
 
 	})
 
