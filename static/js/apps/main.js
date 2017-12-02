fa2d3215fe902b87e5d2b5e71cd14d1929b784fe
ValLR <valentinalopezrecabal@gmail.com>
2017-12-01 22:35:32 -0300

    printig list

--- static/js/apps/main.js
+++ static/js/apps/main.js
@@ -2,7 +2,26 @@ $(document).ready(function() {
 	catalog.forEach(function(el){
 		console.log(el.id);
 		console.log(el.name);
+		console.log(el.description)
 		console.log(el.price);
 		console.log(el.currency);
+		var allProducts = `
+					<div class="center-align col l2 m2 s6">
+						<div class="row">
+							<img src="`+ el.imageURL +`" alt="image`+ el.id +`">
+						</div>
+						<div class="row">
+							<b>`+ el.name +`</b><br>
+							<p>`+ el.description +`</p><br>
+							<b>`+ el.currency + el.price+`</b>
+						</div>
+						<div class="row add">
+							<a class="waves-effect waves-light btn"><i class="material-icons left">add_shopping_cart</i>button</a>
+						</div>
+					</div>
+		`
+		allProducts = $(allProducts);
+		$("#product-container").append(allProducts);
+
 	})
 });
